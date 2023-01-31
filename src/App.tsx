1f20d38501946cac9ea1b8e4cecb5d0a975a6d40
blozinski056 <bloz056111@gmail.com>
2023-01-31 09:40:08 -0800

    start, stop, restart buttons affect timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,71 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 
 import Tiles from "./components/Tiles";
 import { nanoid } from "nanoid";
 
 const App = () => {
-  const [play, setPlay] = useState<boolean>(false);
+  const [time, setTime] = useState<string>("000.000");
+  const [buttonState, setButtonState] = useState<number>(0);
+  const [intervalID, setIntervalID] = useState<NodeJS.Timer>();
 
   const startGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    setPlay(true);
-    let elm = document.querySelector(".start");
-    elm?.setAttribute("disabled", "false");
-    elm?.classList.add("disabled");
+
+    setButtonState(1);
+    // let elm = document.querySelector(".start");
+    // elm?.setAttribute("disabled", "");
+    // elm?.classList.add("disabled");
+
+    const startTime = new Date().getTime();
+    setIntervalID(
+      setInterval(() => {
+        let currTime = new Date().getTime();
+
+        let timeChange = currTime - startTime;
+        let timeNum1: number = Math.floor(timeChange / 1000);
+        let timeNum2: number = timeChange % 1000;
+
+        let timeString1: string = "";
+        let timeString2: string = "";
+
+        if (timeNum1 === 0) {
+          timeString1 = "000";
+        } else if (timeNum1 < 10) {
+          timeString1 = `00${timeNum1}`;
+        } else if (timeNum1 < 100) {
+          timeString1 = `0${timeNum1}`;
+        } else if (timeNum1 < 1000) {
+          timeString1 = timeNum1.toString();
+        } else {
+          setTime("Still playing?");
+          return;
+        }
+
+        if (timeNum2 < 10) {
+          timeString2 = `.00${timeNum2}`;
+        } else if (timeNum2 < 100) {
+          timeString2 = `.0${timeNum2}`;
+        } else if (timeNum2 < 1000) {
+          timeString2 = `.${timeNum2.toString()}`;
+        }
+
+        setTime("".concat(timeString1, timeString2));
+      })
+    );
   };
 
-  const restartGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const stopGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    setPlay(true);
-    // let elm = document.querySelector(".start");
-    // elm?.setAttribute("disabled", "false");
-    // elm?.classList.add("disabled");
+    setButtonState(2);
+    clearInterval(intervalID);
   };
+
+  // const restartGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //   e.preventDefault();
+  //   clearInterval(intervalID);
+  //   startGame(e);
+  // };
 
   const getNewNums = () => {
     let nums: number[] = [];
@@ -49,17 +93,17 @@ const App = () => {
 
   return (
     <>
-      <div className="timer">0000</div>
-      {play ? (
-        <button className="start" onClick={(e) => restartGame(e)}>
-          Restart
-        </button>
-      ) : (
+      <div className="timer">{time}</div>
+      {buttonState !== 1 && (
         <button className="start" onClick={(e) => startGame(e)}>
-          Start!
+          {buttonState === 0 ? "Start!" : "Restart"}
         </button>
       )}
-
+      {buttonState === 1 && (
+        <button className="stop" onClick={(e) => stopGame(e)}>
+          Stop!
+        </button>
+      )}
       <div className="tile-container">{tiles}</div>
       <img src="/images/question.png" alt="" className="rules" />
     </>
