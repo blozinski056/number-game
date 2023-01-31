f6ec6a31657e5e3fb1c21ee95b7f7b963ca74573
blozinski056 <bloz056111@gmail.com>
2023-01-31 15:33:18 -0800

    working with media queries. have to add intervalID to dependencies in timer file without breaking timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import "./App.css";
 
 import Tiles from "./components/Tiles";
@@ -8,10 +8,8 @@ import Timer from "./components/Timer";
 import { nanoid } from "nanoid";
 
 const App = () => {
-  // const [time, setTime] = useState<string>("000.000");
   const [buttonState, setButtonState] = useState<number>(0);
-  const [rules, setRules] = useState<boolean>(false);
-  // const [intervalID, setIntervalID] = useState<NodeJS.Timer>();
+  const [rules, setRules] = useState<boolean>(true);
   const [blankIndex, setBlankIndex] = useState<number>(8);
   const [play, setPlay] = useState<boolean>(false);
 
@@ -36,25 +34,151 @@ const App = () => {
 
   // state to keep track of numbers
   const [nums, setNums] = useState<number[]>(getNewNums());
+  // const [nums, setNums] = useState<number[]>([1, 4, 7, 2, 5, 8, 3, 6, -1]);
 
   // swap tile with blank tile
   const swapTiles = (i: number) => {
-    console.log(i, blankIndex);
+    if (play) {
+      let iRem = i % 3;
+      let bRem = blankIndex % 3;
+      if (
+        (iRem === bRem + 1 && i === blankIndex + 1) ||
+        (iRem === bRem - 1 && i === blankIndex - 1) ||
+        i === blankIndex + 3 ||
+        i === blankIndex - 3
+      ) {
+        let tempNums = nums;
+        let tempTile = tempNums[i];
+        tempNums[i] = tempNums[blankIndex];
+        tempNums[blankIndex] = tempTile;
+        setBlankIndex(i);
+        setNums(tempNums);
+        checkWin();
+      }
+    }
+  };
 
-    let iRem = i % 3;
-    let bRem = blankIndex % 3;
+  // check if win
+  const checkWin = () => {
+    // 1 at i=0, right
     if (
-      (iRem === bRem + 1 && i === blankIndex + 1) ||
-      (iRem === bRem - 1 && i === blankIndex - 1) ||
-      i === blankIndex + 3 ||
-      i === blankIndex - 3
+      nums[0] === 1 &&
+      nums[1] === 2 &&
+      nums[2] === 3 &&
+      nums[3] === 4 &&
+      nums[4] === 5 &&
+      nums[5] === 6 &&
+      nums[6] === 7 &&
+      nums[7] === 8 &&
+      nums[8] === -1
     ) {
-      let tempNums = nums;
-      let tempTile = tempNums[i];
-      tempNums[i] = tempNums[blankIndex];
-      tempNums[blankIndex] = tempTile;
-      setBlankIndex(i);
-      setNums(tempNums);
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=0, down
+    else if (
+      nums[0] === 1 &&
+      nums[1] === 4 &&
+      nums[2] === 7 &&
+      nums[3] === 2 &&
+      nums[4] === 5 &&
+      nums[5] === 8 &&
+      nums[6] === 3 &&
+      nums[7] === 6 &&
+      nums[8] === -1
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=2, left
+    else if (
+      nums[0] === 3 &&
+      nums[1] === 2 &&
+      nums[2] === 1 &&
+      nums[3] === 6 &&
+      nums[4] === 5 &&
+      nums[5] === 4 &&
+      nums[6] === -1 &&
+      nums[7] === 8 &&
+      nums[8] === 7
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=2, down
+    else if (
+      nums[0] === 7 &&
+      nums[1] === 4 &&
+      nums[2] === 1 &&
+      nums[3] === 8 &&
+      nums[4] === 5 &&
+      nums[5] === 2 &&
+      nums[6] === -1 &&
+      nums[7] === 6 &&
+      nums[8] === 3
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=6, right
+    else if (
+      nums[0] === 7 &&
+      nums[1] === 8 &&
+      nums[2] === -1 &&
+      nums[3] === 4 &&
+      nums[4] === 5 &&
+      nums[5] === 6 &&
+      nums[6] === 1 &&
+      nums[7] === 2 &&
+      nums[8] === 3
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=6, up
+    else if (
+      nums[0] === 3 &&
+      nums[1] === 6 &&
+      nums[2] === -1 &&
+      nums[3] === 2 &&
+      nums[4] === 5 &&
+      nums[5] === 8 &&
+      nums[6] === 1 &&
+      nums[7] === 4 &&
+      nums[8] === 7
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=8, left
+    else if (
+      nums[0] === -1 &&
+      nums[1] === 8 &&
+      nums[2] === 7 &&
+      nums[3] === 6 &&
+      nums[4] === 5 &&
+      nums[5] === 4 &&
+      nums[6] === 3 &&
+      nums[7] === 2 &&
+      nums[8] === 1
+    ) {
+      setPlay(false);
+      setButtonState(2);
+    }
+    // 1 at i=8, up
+    else if (
+      nums[0] === -1 &&
+      nums[1] === 6 &&
+      nums[2] === 3 &&
+      nums[3] === 8 &&
+      nums[4] === 5 &&
+      nums[5] === 2 &&
+      nums[6] === 7 &&
+      nums[7] === 4 &&
+      nums[8] === 1
+    ) {
+      setPlay(false);
+      setButtonState(2);
     }
   };
 
@@ -76,71 +200,23 @@ const App = () => {
 
   const startGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-
-    setButtonState(1);
-
     if (buttonState !== 0) {
       setNums(getNewNums());
       setBlankIndex(8);
       tiles = getNewTiles();
     }
-
+    setButtonState(1);
     setPlay(true);
-
-    // const startTime = new Date().getTime();
-    // setIntervalID(
-    //   setInterval(() => {
-    //     let currTime = new Date().getTime();
-
-    //     let timeChange = currTime - startTime;
-    //     let timeNum1: number = Math.floor(timeChange / 1000);
-    //     let timeNum2: number = timeChange % 1000;
-
-    //     let timeString1: string = "";
-    //     let timeString2: string = "";
-
-    //     if (timeNum1 === 0) {
-    //       timeString1 = "000";
-    //     } else if (timeNum1 < 10) {
-    //       timeString1 = `00${timeNum1}`;
-    //     } else if (timeNum1 < 100) {
-    //       timeString1 = `0${timeNum1}`;
-    //     } else if (timeNum1 < 1000) {
-    //       timeString1 = timeNum1.toString();
-    //     } else {
-    //       setTime("Still playing?");
-    //       return;
-    //     }
-
-    //     if (timeNum2 < 10) {
-    //       timeString2 = `.00${timeNum2}`;
-    //     } else if (timeNum2 < 100) {
-    //       timeString2 = `.0${timeNum2}`;
-    //     } else if (timeNum2 < 1000) {
-    //       timeString2 = `.${timeNum2.toString()}`;
-    //     }
-
-    //     setTime("".concat(timeString1, timeString2));
-    //   })
-    // );
-    // clearInterval(intervalID);
   };
 
   const stopGame = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     setButtonState(2);
-    // clearInterval(intervalID);
     setPlay(false);
-  };
-
-  const showRules = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-    e.preventDefault();
-    setRules((prevRules) => !prevRules);
   };
 
   return (
     <>
-      {/* <div className="timer">{time}</div> */}
       <Timer play={play} />
       {buttonState !== 1 && (
         <button className="start" onClick={(e) => startGame(e)}>
@@ -157,18 +233,32 @@ const App = () => {
         src="/images/question.png"
         alt=""
         className="rules"
-        onClick={(e) => showRules(e)}
+        onClick={() => setRules((prevRules) => !prevRules)}
       />
       {rules && (
         <div className="rules-dropdown">
-          <ul>
-            <li>
-              Rearrange the numbers in order with "1" and the blank tile in
-              opposite corners
-            </li>
-            <li></li>
-          </ul>
+          <p>
+            Goal: <br />
+            Re-arrange the tiles so that they look like any orientation of the
+            following examples, where "1" and the blank tile are in opposite
+            corners
+          </p>
+          <div>
+            <img src="/images/solution1.jpg" alt="" />
+            <img src="/images/solution2.jpg" alt="" />
+          </div>
+          <p>
+            How: <br />
+            Click on a tile to move it to the blank space. The tile must be
+            adjacent, not including diagonal
+          </p>
         </div>
+      )}
+      {rules && (
+        <div
+          className="overlay"
+          onClick={() => setRules((prevRules) => !prevRules)}
+        ></div>
       )}
     </>
   );
